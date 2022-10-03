781c11fe64a618e5f12389d848b4fb5f04281f98
Liam <39679453+Wgelyjr@users.noreply.github.com>
2022-10-03 12:46:36 -0500

    Update config.py
    scrubbing info

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'poopy'
+    SECRET_KEY = 'Peter Piper picked a peck of pickled peppers!@#'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
 
